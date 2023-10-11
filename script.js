59c6ce1e75316d30f3d983b0ef05766b7362be53
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-10-11 13:36:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -31,11 +31,19 @@ const lookup = {
   ",": ",",
 };
 
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
+function rot13(str) {
+  str = str.toUpperCase();
+	let op="";// Your Result goes here
   // Only change code below this line
-
-  return; //return decodedArr
+	for(let i=0;i<str.length;i++){
+		if(lookup[str[i]]){
+			op += lookup[str[i]];
+		}
+		else{
+			op += str[i];
+		}
+	}
+  return op; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
